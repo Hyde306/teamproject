f4e28fe2e6413d46e970b9c3c338d6bc35112b27
DESKTOP-CMFB67U\GA2A <h1202419004@hiratagakuen.onmicrosoft.com>
2025-05-22 14:26:24 +0900

    スクリプトにコメントを追加

--- Flip＆Draw/Assets/Othello/Scripts/Core/Coin/Coin.cs
+++ Flip＆Draw/Assets/Othello/Scripts/Core/Coin/Coin.cs
@@ -1,21 +1,35 @@
 using Lacobus.Animation;
 using UnityEngine;
 
-
+/// <summary>
+/// コイントークンの制御を行うクラス。
+/// 表と裏（白と黒）の切り替えやアニメーション・サウンドの再生を行う。
+/// </summary>
 public class Coin : MonoBehaviour
 {
-    // Fields
+    // ----------------------
+    // フィールド
+    // ----------------------
 
+    // 現在のコインの面（白 or 黒）
     [SerializeField] private CoinFace _currentFace;
 
+    // アニメーション処理用のコンポーネント
     private AnimationHandlerComponent _animationHandler;
+
+    // サウンド再生処理用のコンポーネント
     private CoinSoundHandler _soundHandler;
 
+    // ----------------------
+    // パブリックメソッド
+    // ----------------------
 
-    // Public methods
-
+    /// <summary>
+    /// コインの面を反転させる（白 ? 黒）アニメーションとサウンド付き。
+    /// </summary>
     public void FlipFace()
     {
+        // 現在の面を切り替える
         switch (_currentFace)
         {
             case CoinFace.black:
@@ -26,29 +40,46 @@ public class Coin : MonoBehaviour
                 break;
         }
 
+        // 面の変更に応じてアニメーションを再生
         updateRenderer();
+
+        // サウンドを再生
         playSound();
     }
 
+    /// <summary>
+    /// 現在のコインの面を取得する。
+    /// </summary>
+    /// <returns>現在の面（白 or 黒）</returns>
     public CoinFace GetFace()
     {
         return _currentFace;
     }
 
+    // ----------------------
+    // プライベートメソッド
+    // ----------------------
 
-    // Private methods
-
+    /// <summary>
+    /// コンポーネントの初期化処理。
+    /// </summary>
     private void Awake()
     {
         _animationHandler = GetComponent<AnimationHandlerComponent>();
         _soundHandler = GetComponent<CoinSoundHandler>();
     }
 
+    /// <summary>
+    /// ゲーム開始時にコイン設置音を再生。
+    /// </summary>
     private void Start()
     {
         _soundHandler.PlayCoinPlaceSound();
     }
 
+    /// <summary>
+    /// 面の変化に応じたアニメーションを再生。
+    /// </summary>
     private void updateRenderer()
     {
         switch (_currentFace)
@@ -62,14 +93,20 @@ public class Coin : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// コイン反転時のサウンドを再生。
+    /// </summary>
     private void playSound()
     {
         _soundHandler.PlayCoinFlipSound();
     }
 }
 
+/// <summary>
+/// コインの面を表す列挙体。
+/// </summary>
 public enum CoinFace
 {
-    black,
-    white
+    black, // 黒面
+    white  // 白面
 }
--- Flip＆Draw/Assets/Othello/Scripts/Core/Game Director/GameDirector.cs
+++ Flip＆Draw/Assets/Othello/Scripts/Core/Game Director/GameDirector.cs
@@ -1,45 +1,82 @@
 ﻿using UnityEngine;
 
-
+/// <summary>
+/// ゲーム全体の進行を管理するディレクタークラス。
+/// プレイヤーのターン管理や、ゲームの終了判定を行う。
+/// </summary>
 public class GameDirector : MonoBehaviour
 {
-    // Fields
+    // ----------------------
+    // フィールド
+    // ----------------------
 
+    // ボードの参照（盤面管理）
     [SerializeField] private Board _board;
 
+    // 現在のプレイヤーを切り替えるためのフラグ（false = 黒, true = 白）
     private bool _playerSelector = false;
+
+    // ゲーム終了フラグ
     private bool _isGameOver = false;
 
+    // ----------------------
+    // パブリックメソッド
+    // ----------------------
 
-    // Public methods
-
+    /// <summary>
+    /// ゲームが終了しているかどうかを取得する。
+    /// </summary>
+    /// <returns>ゲーム終了時は true</returns>
     public bool IsGameOver() => _isGameOver;
 
+    // ----------------------
+    // プライベートメソッド
+    // ----------------------
 
-    // Private methods
-
+    /// <summary>
+    /// 毎フレーム呼ばれる更新処理。
+    /// プレイヤーの入力・ターン処理・ゲーム終了判定を行う。
+    /// </summary>
     private void Update()
     {
+        // ゲームが終了していなければ処理を続行
         if (!_isGameOver)
+        {
+            // ボードがプレイ可能状態か確認
             if (_board.CanPlay())
             {
+                // 現在のプレイヤーの合法手が更新され、かつボードが満杯でない場合
                 if (_board.UpdateEligiblePositions(getFace()) && !_board.IsFull())
                 {
+                    // マウスの左クリックでコインを置いた場合
                     if (getInput() && _board.PlaceCoinOnBoard(getFace()))
                     {
+                        // プレイヤーを交代
                         _playerSelector = !_playerSelector;
                     }
                 }
                 else
+                {
+                    // 置ける場所がない、またはボードが満杯なのでゲーム終了
                     _isGameOver = true;
+                }
             }
+        }
     }
 
+    /// <summary>
+    /// 入力を取得する（左クリックが押されたかどうか）。
+    /// </summary>
+    /// <returns>クリックされた場合は true</returns>
     private bool getInput()
     {
-        return Input.GetMouseButtonDown(0);
+        return Input.GetMouseButtonDown(0); // 0 は左クリック
     }
 
+    /// <summary>
+    /// 現在のプレイヤーのコイン面（黒 or 白）を取得。
+    /// </summary>
+    /// <returns>現在のプレイヤーの CoinFace</returns>
     private CoinFace getFace()
     {
         return _playerSelector ? CoinFace.white : CoinFace.black;
