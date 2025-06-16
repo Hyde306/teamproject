3929f44b8cbc12f2276d17ace36c3d20df91cf4d
DESKTOP-CMFB67U\GA2A <h1202419004@hiratagakuen.onmicrosoft.com>
2025-06-16 14:48:44 +0900

    no message

---
+++ Flip＆Draw/Assets/nakanishi/CoinFlipButton.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class CoinFlipButton : MonoBehaviour
+{
+    [SerializeField] private Coin targetCoin;
+
+    /// <summary>
+    /// UIボタンから呼び出す用の関数
+    /// </summary>
+    public void OnFlipButtonPressed()
+    {
+        if (targetCoin != null)
+        {
+            targetCoin.FlipFace();
+        }
+    }
+}
